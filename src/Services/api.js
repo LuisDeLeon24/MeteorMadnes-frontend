a7f5756e1bf4bc5fbac9491e9c80c5c57cdcc7d5
LuisDeLeon24 <168132018+LuisDeLeon24@users.noreply.github.com>
2025-10-09 22:36:00 -0600

    more server issues

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/MeteorMadnes",
+  baseURL: "https://meteormadnes-backend.onrender.com",
   timeout: 1000000,
 });
 
